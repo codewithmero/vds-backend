2476e8088dc622bccb52b4a3b5d54127f887d5b5
thecodecrayon <coffeeandfables@gmail.com>
2024-01-21 16:39:47 +0530

    created mongodb database connection files for the project.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import express from 'express';
+import connectDB from './db/index.js';
+
+dotenv.config({ config: './env' });
+
+connectDB();
+
+
+
+
+/* 
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("Application not able to talk to the database:::", error);
+            throw error;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server listening on http://localhost:${process.env.PORT}`);
+        });
+    } catch(error) {
+        console.error("Error (while connecting with database):::", error);
+    }   
+})();
+*/
