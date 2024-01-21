67c40ef3a39f70ae7ced9d5da2b0342aa0f97581
thecodecrayon <coffeeandfables@gmail.com>
2024-01-21 19:05:02 +0530

    added middlewares in the application file.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import express from 'express';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({ config: './env' });
-
-connectDB();
-
-
-
-
-/* 
-;( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", (error) => {
-            console.log("Application not able to talk to the database:::", error);
-            throw error;
-        });
-
-        app.listen(process.env.PORT, () => {
-            console.log(`Server listening on http://localhost:${process.env.PORT}`);
-        });
-    } catch(error) {
-        console.error("Error (while connecting with database):::", error);
-    }   
-})();
-*/
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server listening on http://localhost/${process.env.PORT || 8000}`)
+        })
+    })
+    .catch(err => console.log("MONGODB CONNECTION FAILED!!!", err));
