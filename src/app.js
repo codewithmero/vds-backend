be349c2f1f41a4eeb98f3d766075a1110d52d59f
thecodecrayon <coffeeandfables@gmail.com>
2024-01-22 08:59:43 +0530

    All setup cofigured.

--- src/app.js
+++ src/app.js
@@ -18,4 +18,8 @@ app.use(express.urlencoded({
 }));
 app.use(express.static('public'));
 
+// IMPORTING ROUTES;
+import userRouter from './routes/user.routes.js';
+app.use("/api/v1/users", userRouter);
+
 export { app };
